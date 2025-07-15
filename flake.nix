cd3b32cee98a29b3599c2cfc18a526d6f40d888a
Sepehr Heydari <59925633+GiantSnowMonkey@users.noreply.github.com>
2025-07-14 22:16:26 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
     packages.x86_64-linux = let
       pkgs = import nixpkgs {system = "x86_64-linux";};
     in {
-      thorium-avx = let
+      thorium-avx2 = let
         pkgs = import nixpkgs {system = "x86_64-linux";};
-        name = "thorium-avx";
-        version = "130.0.6723.174 - 62";
+        name = "thorium-avx2";
+        version = "122.0.6261.132 - 56";
         src = pkgs.fetchurl {
           url = "https://github.com/Alex313031/thorium/releases/download/M130.0.6723.174/Thorium_Browser_130.0.6723.174_AVX.AppImage";
           sha256 = "sha256-123ece21d02361199a0e5bf9e80354e69b1cbb070110179ee953d903715dc6c4";
@@ -33,49 +33,16 @@
         };
 
       # AVX is compatible with most CPUs
-      default = self.packages.x86_64-linux.thorium-avx;
+      default = self.packages.x86_64-linux.thorium-avx2;
     };
 
+    apps.x86_64-linux = {
       thorium-avx2 = {
         type = "app";
         program = "${self.packages.x86_64-linux.thorium-avx2}/bin/thorium-avx2";
       };
 
-      default = self.apps.x86_64-linux.thorium-avx;
-    };
-
-    ##### aarch64-linux #####
-    packages.aarch64-linux = {
-      thorium = let
-        pkgs = import nixpkgs {system = "aarch64-linux";};
-        name = "thorium";
-        version = "122.0.6261.132 - 6";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Alex313031/Thorium-Raspi/releases/download/M122.0.6261.132/Thorium_Browser_122.0.6261.132_arm64.AppImage";
-          sha256 = "sha256-gS3/f7wq5adOLZuS2T8SWfme/Z1bFqHSpMLUsENKlcw=";
-        };
-        appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
-      in
-        pkgs.appimageTools.wrapType2 {
-          inherit name version src;
-          extraInstallCommands = ''
-            install -m 444 -D ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
-            install -m 444 -D ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
-            substituteInPlace $out/share/applications/thorium-browser.desktop \
-            --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${name} %U'
-          '';
-        };
-
-      default = self.packages.aarch64-linux.thorium;
-    };
-
-    apps.aarch64-linux = {
-      thorium = {
-        type = "app";
-        program = "${self.packages.aarch64-linux.thorium}/bin/thorium";
-      };
-
-      default = self.apps.aarch64-linux.thorium;
+      default = self.apps.x86_64-linux.thorium-avx2;
     };
   };
 }
