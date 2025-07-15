4a9c619463ede4f99c9ab1a0551cb3c41ab7ba66
Sepehr Heydari <59925633+GiantSnowMonkey@users.noreply.github.com>
2025-07-14 22:09:07 -0400

    Update flake.nix
    Updated to latest avx2

--- flake.nix
+++ flake.nix
@@ -15,50 +15,10 @@
       thorium-avx = let
         pkgs = import nixpkgs {system = "x86_64-linux";};
         name = "thorium-avx";
-        version = "122.0.6261.132 - 56";
+        version = "130.0.6723.174 - 62";
         src = pkgs.fetchurl {
-          url = "https://github.com/Alex313031/thorium/releases/download/M122.0.6261.132/thorium_browser_122.0.6261.132_AVX.AppImage";
-          sha256 = "sha256-2PJxnKzppjHrYQnGYYe1BG0075FwDdnjY0JI2X5AIvQ=";
-        };
-        appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
-      in
-        pkgs.appimageTools.wrapType2 {
-          inherit name version src;
-          extraInstallCommands = ''
-            install -m 444 -D ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
-            install -m 444 -D ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
-            substituteInPlace $out/share/applications/thorium-browser.desktop \
-            --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${name} %U'
-          '';
-        };
-
-      thorium-avx2 = let
-        pkgs = import nixpkgs {system = "x86_64-linux";};
-        name = "thorium-avx2";
-        version = "122.0.6261.132 - 56";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Alex313031/thorium/releases/download/M122.0.6261.132/thorium_browser_122.0.6261.132_AVX2.AppImage";
-          sha256 = "sha256-HANrDUv/oFW2uWLSYilTCzdnZDY1yuqhLo/jRQil3QA=";
-        };
-        appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
-      in
-        pkgs.appimageTools.wrapType2 {
-          inherit name version src;
-          extraInstallCommands = ''
-            install -m 444 -D ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
-            install -m 444 -D ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
-            substituteInPlace $out/share/applications/thorium-browser.desktop \
-            --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${name} %U'
-          '';
-        };
-
-      thorium-sse3 = let
-        pkgs = import nixpkgs {system = "x86_64-linux";};
-        name = "thorium-sse3";
-        version = "122.0.6261.132 - 56";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Alex313031/thorium/releases/download/M122.0.6261.132/thorium_browser_122.0.6261.132_SSE3.AppImage";
-          sha256 = "sha256-G+Z85w7d7YT/03tqcH1VMJGoenoegcttbxz38u0JWcI=";
+          url = "https://github.com/Alex313031/thorium/releases/download/M130.0.6723.174/Thorium_Browser_130.0.6723.174_AVX.AppImage";
+          sha256 = "sha256-123ece21d02361199a0e5bf9e80354e69b1cbb070110179ee953d903715dc6c4";
         };
         appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
       in
@@ -76,20 +36,9 @@
       default = self.packages.x86_64-linux.thorium-avx;
     };
 
-    apps.x86_64-linux = {
-      thorium-avx = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.thorium-avx}/bin/thorium-avx";
-      };
-
       thorium-avx2 = {
         type = "app";
         program = "${self.packages.x86_64-linux.thorium-avx2}/bin/thorium-avx2";
-      };
-
-      thorium-sse3 = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.thorium-sse3}/bin/thorium-sse3";
       };
 
       default = self.apps.x86_64-linux.thorium-avx;
